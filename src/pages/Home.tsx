4c676a21d8df7fa1a07f6be5da2fae3633e05d5b
Leandro Araujo <leandroaraujo5511@gmail.com>
2022-01-06 15:53:33 -0300

    Implementação da Tela de autenticação

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,43 @@
 
+import illustrationImg from '../assets/images/illustration.svg'
+import logoImg from '../assets/images/logo.svg'
+import logoGoogleImg from '../assets/images/google-icon.svg'
+
+
+import '../styles/auth.scss'
+import { Button } from '../components/Button'
+
 
 export function Home() {
-    return{
-        
-    }
+    return(
+        <div id='page-auth'>
+            <aside>
+                <img src={illustrationImg} alt="Ilustração simbolizando perguntas e respostas" />
+
+                <strong>Toda pergunta tem uma resposta.</strong>
+                <p>Aprenda e compartilhe conhecimento com outras pessoas</p>
+            </aside>
+
+            <main>
+                <div className='main-content'>
+                    <img src={logoImg} alt="LetMeAsk"  />
+                    <button className='create-room'>
+                        <img src={logoGoogleImg} alt="Logo do Google" />
+                        Crie sua sala com o Google
+                    </button>
+                    <div className='separation'>ou entre em uma sala</div>
+                    <form>
+                        <input 
+                            type="text"
+                            placeholder='Digite o código da sala'
+                        />
+                        <Button type='submit'>
+                            Entrar na sala
+                        </Button>
+
+                    </form>
+                </div>
+            </main>
+        </div>
+    )
 }
